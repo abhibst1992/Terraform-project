e67c603aaa2e9d4b831c0932b3129adea432ae87
abhibst1992 <108710681+abhibst1992@users.noreply.github.com>
2022-08-04 10:26:47 +0530

    Create aws.tf

---
+++ aws.tf
@@ -0,0 +1,12 @@
+# Create a simple ec2 instance
+resource "aws_instance" "Abhi" {
+    ami = "ami-076e3a557efe1aa9c"
+    instance_type = "t2.micro" 
+    count = 1
+    subnet_id = "subnet-0d47c7cc17ec8f644"
+
+    Tags = {
+       "name" = "Abhi"
+    }
+
+}
